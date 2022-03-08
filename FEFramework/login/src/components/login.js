008dab426ab09116eb2e01fa61e1410ba15830cb
Monique <monique.senjaya@binus.ac.id>
2022-03-08 16:25:01 +0700

    forum ans

---
+++ FEFramework/login/src/components/login.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import App from '../App';
+
+const Login=()=>{
+    return(
+        <>
+        <Grid>
+            <Paper elevation={10} className="p-20 h-[70vh] w-[400px] m-20">
+                <Grid className='items-center w-100% flex flex-col'>
+                     <Avatar className='bg-[#1bbd7e]'><LockOutlinedIcon/></Avatar>
+                    <h2>Sign In</h2>
+                </Grid>
+                <TextField label='Username' placeholder='Enter username' fullWidth required/>
+                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
+                <FormControlLabel
+                    control={
+                    <Checkbox
+                        name="checkedB"
+                        color="primary"
+                    />
+                    }
+                    label="Remember me"
+                 />
+                <Button type='submit' color='primary' variant="contained" fullWidth className='mx-8'>Sign in</Button>
+                <Typography >
+                     <Link href="#" >
+                        Forgot password ?
+                </Link>
+                </Typography>
+                <Typography > Do you have an account ?
+                     <Link href="#" >
+                        Sign Up 
+                </Link>
+                </Typography>
+            </Paper>
+        </Grid>
+        </>
+    )
+}
+
+export default Login;
